e17bb82aa8ace2f2556472e0ef98ab727e231ea4
Vanshika <agarwalvanshika109@gmail.com>
2023-06-11 13:17:22 +0530

    conversions

---
+++ NestedRecursion/DtoB.java
@@ -0,0 +1,30 @@
+// Java program for decimal to binary
+// conversion using recursion
+import java.io.*;
+
+public class DtoB {
+
+	
+	// Decimal to binary conversion
+	// using recursion
+	static int find(int decimal_number)
+	{
+		if (decimal_number == 0)
+			return 0;
+			
+		else
+		
+		return (decimal_number % 2 + 10 *
+				find(decimal_number / 2));
+	}
+	
+// Driver Code
+public static void main(String args[])
+{
+	int decimal_number = 10;
+	System.out.println(find(decimal_number));
+}
+
+}
+
+// This code is contributed by Nikita Tiwari
---
+++ NestedRecursion/HtoB.java
@@ -0,0 +1,88 @@
+// Java program to convert Hexadecimal to Binary
+
+import java.util.HashMap;
+
+public class HtoB {
+
+	// declaring the method to convert
+	// Hexadecimal to Binary
+	String hexToBinary(String hex)
+	{
+
+		// variable to store the converted
+		// Binary Sequence
+		String binary = "";
+
+		// converting the accepted Hexadecimal
+		// string to upper case
+		hex = hex.toUpperCase();
+
+		// initializing the HashMap class
+		HashMap<Character, String> hashMap
+			= new HashMap<Character, String>();
+
+		// storing the key value pairs
+		hashMap.put('0', "0000");
+		hashMap.put('1', "0001");
+		hashMap.put('2', "0010");
+		hashMap.put('3', "0011");
+		hashMap.put('4', "0100");
+		hashMap.put('5', "0101");
+		hashMap.put('6', "0110");
+		hashMap.put('7', "0111");
+		hashMap.put('8', "1000");
+		hashMap.put('9', "1001");
+		hashMap.put('A', "1010");
+		hashMap.put('B', "1011");
+		hashMap.put('C', "1100");
+		hashMap.put('D', "1101");
+		hashMap.put('E', "1110");
+		hashMap.put('F', "1111");
+
+		int i;
+		char ch;
+
+		// loop to iterate through the length
+		// of the Hexadecimal String
+		for (i = 0; i < hex.length(); i++) {
+			// extracting each character
+			ch = hex.charAt(i);
+
+			// checking if the character is
+			// present in the keys
+			if (hashMap.containsKey(ch))
+
+				// adding to the Binary Sequence
+				// the corresponding value of
+				// the key
+				binary += hashMap.get(ch);
+
+			// returning Invalid Hexadecimal
+			// String if the character is
+			// not present in the keys
+			else {
+				binary = "Invalid Hexadecimal String";
+				return binary;
+			}
+		}
+
+		// returning the converted Binary
+		return binary;
+	}
+
+	// Driver Code
+	public static void main(String[] args)
+	{
+
+		// instantiating the class
+		GFG ob = new GFG();
+	
+		String hex = "deafa";
+	
+		System.out.println(hex.toUpperCase());
+	
+		// printing and calling the
+		// hexToBinary() function
+		System.out.println(ob.hexToBinary(hex));
+	}
+}
