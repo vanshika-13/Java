58af8e48e55510b5514929a18339c062e783a1d0
Vanshika <agarwalvanshika109@gmail.com>
2023-06-30 11:13:32 +0530

    bits

---
+++ bit/maxNum.java
@@ -0,0 +1,18 @@
+package bit;
+
+public class maxNum {
+    static int help(int n) {
+        int cnt = 0;
+        for (int x = 0; x <= n; x++) {
+            int res = n & x;
+            if (res == x) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(help(5));
+    }
+}
