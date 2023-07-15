89a2078eb249a51392508c6f1d61e22a30d2f506
Vanshika <agarwalvanshika109@gmail.com>
2023-07-15 22:34:36 +0530

    changes

---
+++ ArrayMid/matrixSum.java
@@ -0,0 +1,21 @@
+public class matrixSum {
+    public void Addition(int[][] matrixA, int[][] matrixB)
+
+    {
+
+        for (int i = 0; i < matrixA.length; i++)
+
+        {
+
+            for (int j = 0; j < matrixA[0].length; j++)
+
+            {
+
+                matrixA[i][j] += matrixB[i][j];
+
+            }
+
+        }
+
+    }
+}
---
+++ LinkedList/Cycle.java
@@ -0,0 +1,32 @@
+package LinkedList;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Cycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow)
+                return true;
+        }
+        return false;
+
+    }
+}
